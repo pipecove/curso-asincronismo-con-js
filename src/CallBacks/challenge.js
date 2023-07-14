d99e0c6bf6fc4a8d306b8b6e3d252d0aec4001de
pipecove <pipecove@gmail.com>
2023-07-14 02:58:20 -0500

    Creamos es sript para ejecutar el callback desde la terminal modificando el package.json

--- src/CallBacks/challenge.js
+++ src/CallBacks/challenge.js
@@ -20,13 +20,14 @@ function fetchData(urlApi, callback){             //permite recibir la url y el 
 
 fetchData(`${API}/products`, function (error1, data1){
     if (error1) return console.error(error1);
-    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2){
+    fetchData(`${API}/products/${data1[1].id}`, function (error2, data2){
         if (error2) return console.error(error2);
-        fetchData(`${API}/categories/${data2?.category?.id}`, function (error3, data3){
+        fetchData(`${API}/categories/${data2.category.id}`, function (error3, data3){
             if (error3) return console.error(error3);
             console.log(data1[0]);
             console.log(data2.title);
-            console.log(data3.name)
+            console.log(data3.name);
         });
     });
 });
+
